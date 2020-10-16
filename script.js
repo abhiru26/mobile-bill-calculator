a29ff688d7d8cab0b6760b0d50097b7202c0d526
Abhirup Hazra <abhi.cu06@gmail.com>
2020-10-16 14:26:55 +0530

    version 1.0

---
+++ script.js
@@ -0,0 +1,18 @@
+function durationvalue(){
+
+    var duartion = parseInt( document.getElementById("duration").value);
+    duartion = duartion + 5;
+
+
+   
+   
+    document.write("Duration is: " + duartion + "<br>");
+    document.write("scheme type is:"+"<br>");
+    document.write("broadband type:"+"<br>");
+    document.write("your bill is = "+"<br>");
+    
+    
+    
+    
+}
+
